43b62a9831a93d4d7e173d7459aa4c1c9b31ff77
erischon <66017574+erischon@users.noreply.github.com>
2023-10-27 10:02:47 +0200

    sidebar, mobile sidebar and dashboard

--- src/app/(dashboard)/(routes)/layout.tsx
+++ src/app/(dashboard)/(routes)/layout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "@/components/ui/Navbar";
-import Sidebar from "@/components/ui/Sidebar";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 export default function DashboardLayout({
   children,
@@ -8,7 +8,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-stone-900">
+      <div className="hidden h-full md:flex md:w-60 md:flex-col md:fixed md:inset-y-0 z-[80] bg-stone-900">
         <Sidebar />
       </div>
 
